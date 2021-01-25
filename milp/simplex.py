974e0d13e16777702468611f963d6eed30cf84de
Horace Pan <hopan@uchicago.edu>
2021-01-25 04:01:47 -0500

    simplex bug fixes

--- milp/simplex.py
+++ milp/simplex.py
@@ -1,22 +1,23 @@
+import sys
 import pdb
 import numpy as np
 
 class Simplex:
-    def __init__(self, A, b, c, const=0):
+    def __init__(self, A, b, c, const=0, mode="maximize"):
         A_s, c_s = self._add_slack_vars(A, c)
         self.A = A
         self.b = b
         self.c = c
-        self.const = 0
+        self.const = const
+        self.mode = mode
 
         self.A_tableau = A_s
         self.c_tableau = c_s
         self.b_tableau = b
         self.tableau_const = const
-        #self._nonbasic_vars = [i for i in range(A.shape[0])]
-        #self._basic_vars = [i for i in range(A.shape[0], self.A_tableau[0])] # start off as the slack variables
-        self.basic_vars = list(range(A.shape[0], self.A_tableau.shape[0]))
-        self.nonbasic_vars = list(range(A.shape[0]))
+        self.basic_vars = list(range(A.shape[1], self.A_tableau.shape[1]))
+        self._sol = np.zeros(len(c_s))
+        self._sol[A.shape[1]:] = self.b_tableau
 
     def _add_slack_vars(self, A, c):
         nslack = A.shape[0]
@@ -25,13 +26,27 @@ class Simplex:
         return A_new, c_new
 
     def pivot(self):
-        i, j = self.find_pivot_max()
-        print("Pivoting on x[", j, "] x[j] =", self.c_tableau[j], "using row:", i, "aij:", self.A_tableau[i, j])
+        if self.mode == "maximize":
+            i, j = self.find_pivot_max()
+        elif self.mode == "minimize":
+            i, j = self.find_pivot_min()
+        else:
+            raise Exception(f"{self.mode} is not a valid mode for simplex")
+
+        #print(f"{self.basic_vars[i]+1} is leaving, {j+1} is entering")
+
+        constraint = i
+        try:
+            leaving_var = self.basic_vars[i]
+        except:
+            pdb.set_trace()
+        entering_var = j
+
+        self.basic_vars[i] = j
         a_ij = self.A_tableau[i, j]
         row_i = self.A_tableau[i, :]
         row_i_normed = row_i / a_ij
         b_i_normed = self.b_tableau[i] / a_ij
-        #print("a_ij:", a_ij, "b normed:", b_i_normed)
 
         for k in range(self.A_tableau.shape[0]):
             if k == i:
@@ -47,6 +62,10 @@ class Simplex:
         c_j = self.c_tableau[j]
         self.c_tableau -= c_j * row_i_normed
         self.tableau_const -= c_j*b_i_normed
+
+        self._sol[:] = 0
+        for idx, v in enumerate(self.basic_vars):
+            self._sol[v] = self.b_tableau[idx]
 
     def find_pivot_max(self, rule='lex'):
         idx = None
@@ -65,21 +84,44 @@ class Simplex:
                     min_row = row_idx
                     eps = val
 
-        print("finding best pivot", self.A_tableau[:, idx])
         return min_row, idx
 
     def find_pivot_min(self):
+        idx = None
+        for i, _c in enumerate(self.c_tableau):
+            if _c < 0:
+                idx = i
+                break
+
+        # find the row i such that b_i / a_ij is minimal
+        eps = float("inf")
+        min_row = None
+        for row_idx in range(self.A_tableau.shape[0]):
+            if self.A_tableau[row_idx, idx] > 0:
+                val = self.b_tableau[row_idx] / self.A_tableau[row_idx, idx]
+                if val < eps:
+                    min_row = row_idx
+                    eps = val
+
+        return min_row, idx
+
+    def obj_val(self):
+        return (self._sol[:len(self.c)] * self.c).sum() + self.const
+
+    def sol(self):
+        return self._sol
+
+    def sol2(self):
+        sol = np.zeros(len(self.c))
+
+        for idx, i in enumerate(self.basic_vars):
+            if i < len(sol):
+                sol[i] = self.b_tableau[idx]
+        return sol
+
+    def add_cut(self, cut):
+        # this may expand the tableau
         pass
-
-    def _do_basis(self, basis, A, b, c):
-        A_B = A[:, basis]
-        A_B_T = A_B.T
-        A_B_inv = np.linalg.inv(A_B)
-        A_B_T_inv = np.linalg.inv(A_B_T)
-
-        y = A_B_T_inv @ c[basis]
-        b_bar = A_B_inv @ b
-        A_bar = A_B_inv @ A
 
     def current_tableau(self):
         return self._tableau
@@ -96,57 +138,72 @@ class Simplex:
     def solve(self):
         sol = 0
         iters = 0
-        while not np.all(self.c_tableau <= 0):
-            self.pivot()
-            self.ppt()
-            iters += 1
-        print(f"Solved after {iters} iters")
+        self.ppt()
 
-    def pp(self):
-        for ridx, row in enumerate(self._tableau):
-            line = ""
-            if ridx == 0:
-                # first row of the tableau is the objective row
-                continue
+        if self.mode == "maximize":
+            while not np.all(self.c_tableau <= 0):
+                self.pivot()
+                self.ppt()
+                iters += 1
+            #print(f"Solved after {iters} iters")
+        elif self.mode == "minimize":
+            while not np.all(self.c_tableau >= 0):
+                self.pivot()
+                self.ppt()
+                iters += 1
+            #print(f"Solved after {iters} iters | basic vars:", self.basic_vars)
+        return self.tableau_const
 
-            for idx, val in enumerate(row):
-                if idx > 0:
-                    line += f"+ {val}x{idx+1}"
-                elif idx < len(row) - 1:
-                    line += f"<= {val}"
-                else:
-                    line += f"{val}x{idx+1}"
-
-            line += " <= "
-            line += self.tableau_b[-1]
-        print(line)
-
-    def add_cut(self, a, b):
+    def add_constraint(self, ai, bi):
         '''
         Add new constraint to the problem
         '''
-        pass
+        A_new = np.append(self.A, a_i.reshape(1, -1))
+        b_new = np.append(self.b, bi)
 
     def ppt(self):
         print('const:', self.tableau_const)
-        print('c:\n',   self.c_tableau)
-        print('A:\n',   self.A_tableau)
-        print('b:\n',   self.b_tableau)
-        print('======================')
+        print('c:')
+        print(self.c_tableau)
+        print('A:')
+        for row in self.A_tableau:
+            print(row)
+        print('b:')
+        print(self.b_tableau)
 
+        sol_vec = np.zeros(self.A_tableau.shape[1])
+        for idx, i in enumerate(self.basic_vars):
+            try:
+                sol_vec[i] = self.b_tableau[idx]
+            except:
+                pdb.set_trace()
+        print("Solution vec:", self._sol)
+        print("Objective: ", self.obj_val())
+        print("==============================")
 
 if __name__ == '__main__':
-    A = np.array([
-        [1, 0, 0],
-        [2, 1, 1],
-        [2, 2, 1],
-    ])
-    b = np.array([4, 10, 16])
-    c = np.array([20, 16, 12])
-    const = -10
-    simplex = Simplex(A, b, c, const)
+    if sys.argv[1] == "max":
+        A = np.array([
+            [1, 0, 0],
+            [2, 1, 1],
+            [2, 2, 1],
+        ])
+        b = np.array([4, 10, 16])
+        c = np.array([20, 16, 12])
+        const = 10
+        mode = "maximize"
+    else:
+        A = np.array([
+            [ 1,  0, 0],
+            [ 0,  1, 0],
+            [ 1,  1, 0],
+            [-1,  0, 2],
+        ])
+        b = np.array([4, 4, 6, 4])
+        c = np.array([-1, 2, -1])
+        const = 0
+        mode = "minimize"
+
+    simplex = Simplex(A, b, c, const, mode)
     simplex.solve()
-    #simplex.pivot()
-    #simplex.ppt()
-    #simplex.pivot()
-    #simplex.ppt()
+    print("Opt val: {}".format(simplex.obj_val()))
