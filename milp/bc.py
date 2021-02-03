0866286053c4e24b38aa8b41260f0c8b3683ab75
Horace Pan <hopan@uchicago.edu>
2021-02-03 16:28:22 -0500

    change to gurobi opt to allow lp/ip solves

--- milp/bc.py
+++ milp/bc.py
@@ -8,11 +8,11 @@ from gurobipy import Model, GRB, quicksum
 
 Result = namedtuple("Result", ("x", "fun", "success"))
 
-def guro_opt(c, A, b, minimize=True):
+def guro_opt(c, A, b, minimize=True, vtype=GRB.CONTINUOUS):
     m = Model()
     m.setParam('OutputFlag', 0)
     mode = GRB.MINIMIZE if minimize else GRB.MAXIMIZE
-    _vars = [m.addVar(lb=0, vtype="C") for i in range(A.shape[1])]
+    _vars = [m.addVar(lb=0, vtype=vtype) for i in range(A.shape[1])]
     m.setObjective(quicksum(_vars[i]*c[i] for i in range(len(c))), mode)
 
     for idx in range(A.shape[0]):
@@ -165,14 +165,35 @@ if __name__ == '__main__':
     ])
     b = np.array([10, 1, 1, 1, 1])
     c = -np.array([15, 12, 4, 2])
+    A = np.array([
+            [-1, 2, -6],
+            [1, 0, 2],
+            [2, 0, 10],
+            [-1, 1, 0]
+    ])
+    b = np.array([
+        -10, 6, 19, -2
+    ])
+    c = np.array([
+        2, 15, 18
+    ])
 
     print("bc:")
-    #A = np.array([
-    #    [-5, 4],
-    #    [5, 2]
-    #])
-    #b = np.array([0, 15])
-    #c = np.array([-1, -1])
+    A = np.array([
+        [-5, 4],
+        [5, 2]
+    ])
+    b = np.array([0, 15])
+    c = np.array([-1, -1])
+    A = np.array([
+        [8, 5, 3, 2],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
+    ])
+    b = np.array([10, 1, 1, 1, 1])
+    c = -np.array([15, 12, 4, 2])
 
     sol, obj = bc(A, b, c, "all_gomory")
     print('sol:', sol)
@@ -181,6 +202,9 @@ if __name__ == '__main__':
     print("Elapsed: {:.2f}s".format(time.time() -st))
     print('===============')
 
+    res = guro_opt(c, A, b, vtype=GRB.INTEGER)
+    print(res)
+    exit()
     st = time.time()
     m = Model()
     m.setParam("OutputFlag", 0)
